301df3d014c804b6f7a52213f59b198820d112d3
Denys Temchenko <dtemchenko@magecore.com>
2015-02-04 16:55:38 +0000

    default bundle generated

---
+++ src/Dte/BtsBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dte\BtsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DteBtsBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Dte/BtsBundle/DteBtsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dte\BtsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DteBtsBundle extends Bundle
+{
+}

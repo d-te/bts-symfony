5ce86a30ba06f7a9d6dbb3b08311450feddc0118
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-06 16:11:42 +0000

    DefaultControllerTest

--- src/Dte/BtsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Dte/BtsBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,61 @@
 namespace Dte\BtsBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    private $client = null;
+
+    public function setUp()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
+    }
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+    public function testIndexWithoutAuth()
+    {
+        $this->client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($this->client->getResponse() instanceof RedirectResponse);
+
+        $this->assertRegExp('/\/login$/', $this->client->getResponse()->headers->get('location'));
+    }
+
+    public function testIndexWithAuth()
+    {
+        $this->logIn();
+
+        $this->client->request('GET', '/');
+
+        $this->assertFalse($this->client->getResponse() instanceof RedirectResponse);
+    }
+
+    public function testLoginForm()
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $this->assertTrue($crawler->filter('h1:contains("Login")')->count() > 0);
+
+        $this->assertTrue($crawler->filter('input[name="_username"]')->count() > 0);
+
+        $this->assertTrue($crawler->filter('input[name="_password"]')->count() > 0);
+
+        $this->assertTrue($crawler->filter('button[type=submit]')->count() > 0);
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 }

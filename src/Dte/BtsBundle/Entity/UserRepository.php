0e2e98628a978c686233c5afa3f4567abece0d3e
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-09 17:21:11 +0000

    namespace fix

--- src/Dte/BtsBundle/Entity/UserRepository.php
+++ src/Dte/BtsBundle/Entity/UserRepository.php
@@ -4,8 +4,8 @@ namespace Dte\BtsBundle\Entity;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;

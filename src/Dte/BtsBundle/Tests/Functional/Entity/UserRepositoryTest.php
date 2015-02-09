d2f08c7ba0bf33cd8536b42c24cfbfc228db0c95
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-09 17:21:48 +0000

    UserRepository test added

---
+++ src/Dte/BtsBundle/Tests/Functional/Entity/UserRepositoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Dte\BtsBundle\Tests\Functional\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+class ProductRepositoryFunctionalTest extends WebTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager()
+        ;
+    }
+
+    public function testImplamantaionUserProviderInterface()
+    {
+        $repository = $this->em->getRepository('DteBtsBundle:User');
+
+        $this->assertTrue($repository instanceOf UserProviderInterface);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->em->close();
+    }
+}

d1a0e721468671683960b23b3e233d19af350895
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-09 14:49:02 +0000

    UserForm updated

--- src/Dte/BtsBundle/Form/UserType.php
+++ src/Dte/BtsBundle/Form/UserType.php
@@ -19,7 +19,7 @@ class UserType extends AbstractType
             ->add('username', 'text', array('required' => true, 'label' => 'Nickname'))
             ->add('fullname', 'text', array('required' => true))
             ->add('password', 'password', array('required' => true))
-            ->add('avatar', 'url')
+            ->add('avatar', 'url', array('required' => false))
         ;
     }
 

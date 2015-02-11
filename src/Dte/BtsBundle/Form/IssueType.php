76834233a2d766f526dd4fa6de4f097725871189
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-11 11:50:07 +0000

    Issue form, templates updated

--- src/Dte/BtsBundle/Form/IssueType.php
+++ src/Dte/BtsBundle/Form/IssueType.php
@@ -17,17 +17,20 @@ class IssueType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $isCreateContext = ($options['form_context'] === 'create');
+        $isEditContext   = ($options['form_context'] === 'edit');
+
         $builder
-            ->add('project', 'entity', array(
-                'required'    => true,
-                'property'    => 'selectLabel',
-                'class'       => 'DteBtsBundle:Project',
-                'empty_value' => 'Select a project',
+            ->add('project', 'entity', array( //TODO only member`s porjects
+                'required'      => true,
+                'property'      => 'selectLabel',
+                'class'         => 'DteBtsBundle:Project',
+                'empty_value'   => 'Select a project',
                 'query_builder' => function(EntityRepository $em) {
                     return $em->createQueryBuilder('p')->orderBy('p.id', 'DESC');
                 },
             ))
-            ->add('code', 'text', array('required' => true)) // autogenrated, not editable
+            ->add('code', 'text', array('required' => false)) // autogenrated, not editable
             ->add('summary', 'textarea', array('required' => true))
             ->add('description', 'textarea', array('required' => false))
             ->add('type', 'choice', array(
@@ -40,47 +43,47 @@ class IssueType extends AbstractType
                 'data' => 2,
             ))
             ->add('parent', 'entity', array( //TODO only stories types, not editable for some types
-                'required'    => false,
-                'property'    => 'label',
-                'class'       => 'DteBtsBundle:Issue',
-                'empty_value' => 'Select an parent issue',
+                'required'      => false,
+                'property'      => 'label',
+                'class'         => 'DteBtsBundle:Issue',
+                'empty_value'   => 'Select an parent issue',
                 'query_builder' => function(EntityRepository $em) {
                     return $em->createQueryBuilder('i')->orderBy('i.id', 'DESC');
                 },
             ))
             ->add('status', 'entity', array(
-                'required' => true,
-                'property' => 'label',
-                'class'    => 'DteBtsBundle:IssueStatus',
+                'required'      => true,
+                'property'      => 'label',
+                'class'         => 'DteBtsBundle:IssueStatus',
                 'query_builder' => function(EntityRepository $em) {
                     return $em->createQueryBuilder('i')->orderBy('i.order', 'ASC');
                 },
 
             ))
             ->add('priority', 'entity', array(
-                'required' => true,
-                'property' => 'label',
-                'class'    => 'DteBtsBundle:IssuePriority',
+                'required'      => true,
+                'property'      => 'label',
+                'class'         => 'DteBtsBundle:IssuePriority',
                 'query_builder' => function(EntityRepository $em) {
-                    return $em->createQueryBuilder('ip')->orderBy('ip.order', 'ASC');
+                    return $em->createQueryBuilder('i')->orderBy('i.order', 'ASC');
                 },
                 'data' => 3,
             ))
-            ->add('assignee', 'entity', array(
-                'required'    => false,
-                'property'    => 'fullname',
-                'class'       => 'DteBtsBundle:User',
-                'empty_value' => 'Select a assignee',
+            ->add('assignee', 'entity', array( //TODO only project Members
+                'required'      => false,
+                'property'      => 'fullname',
+                'class'         => 'DteBtsBundle:User',
+                'empty_value'   => 'Select a assignee',
                 'query_builder' => function(EntityRepository $em) {
                     return $em->createQueryBuilder('u')->orderBy('u.fullname', 'ASC');
                 },
             ))
             ->add('resolution', 'entity', array( //TODO only to resolve|reopen actions
-                'required' => false,
-                'property' => 'label',
-                'class'    => 'DteBtsBundle:IssueResolution',
+                'required'      => false,
+                'property'      => 'label',
+                'class'         => 'DteBtsBundle:IssueResolution',
                 'query_builder' => function(EntityRepository $em) {
-                    return $em->createQueryBuilder('ir')->orderBy('ir.order', 'ASC');
+                    return $em->createQueryBuilder('i')->orderBy('i.order', 'ASC');
                 },
             ))
         ;
@@ -92,7 +95,8 @@ class IssueType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Dte\BtsBundle\Entity\Issue'
+            'data_class'   => 'Dte\BtsBundle\Entity\Issue',
+            'form_context' => 'default',
         ));
     }
 

e273dbe0a2dd2ba687792990827a82cd6ffa3dbf
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-16 13:23:37 +0000

    Log activities functional

---
+++ src/Dte/BtsBundle/EventListener/ActivitySubscriber.php
@@ -0,0 +1,76 @@
+<?php
+namespace Dte\BtsBundle\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
+
+use Dte\BtsBundle\Entity\Comment;
+use Dte\BtsBundle\Entity\Issue;
+use Dte\BtsBundle\Manager\ActivityManager;
+
+class ActivitySubscriber implements EventSubscriber
+{
+    /**
+     * @var ActivityManager
+     */
+    private $activityManager;
+
+    /**
+     * Constructor
+     */
+    public function __construct(ActivityManager $activityManager)
+    {
+        $this->activityManager = $activityManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            'postPersist',
+            'preUpdate',
+            'postFlush',
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $em     = $args->getEntityManager();
+
+        if ($entity instanceof Issue) {
+            $this->activityManager->logPersistIssue($entity);
+        } else if ($entity instanceof Comment) {
+            $this->activityManager->logPersistComment($entity);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $em     = $args->getEntityManager();
+
+        if ($entity instanceof Issue) {
+            if ($args->hasChangedField('status')) {
+                $this->activityManager->logUpdateIssueStatus($entity);
+            }
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        $this->activityManager->saveActivities();
+    }
+}
---
+++ src/Dte/BtsBundle/Manager/ActivityManager.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Dte\BtsBundle\Manager;
+
+use Doctrine\ORM\EntityManager;
+
+use Dte\BtsBundle\Entity\Activity;
+use Dte\BtsBundle\Entity\Comment;
+use Dte\BtsBundle\Entity\Issue;
+use Dte\BtsBundle\Entity\User;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ActivityManager
+{
+    /**
+     *
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Activities to save
+     * @var array
+     */
+    private $activities = array();
+
+    /**
+     * Constructor
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * get current User
+     * @var User
+     */
+    public function getUser()
+    {
+        return $this->container->get('security.context')->getToken()->getUser();
+    }
+
+    /**
+     * get current EntityManager
+     * @var EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->container->get('doctrine')->getEntityManager();
+    }
+
+    /**
+     * Log persist issue
+     *
+     * @param Issue $issue
+     */
+    public function logPersistIssue(Issue $issue)
+    {
+        $message = 'New issue added';
+        $this->addActivity($message, $issue, $this->getUser());
+    }
+
+    /**
+     * Log change status issue
+     *
+     * @param Issue $issue
+     */
+    public function logUpdateIssueStatus(Issue $issue)
+    {
+        $message = 'Issue status changed to ' . $issue->getStatus()->getLabel();
+        $this->addActivity($message, $issue, $this->getUser());
+    }
+
+    /**
+     * Log persist comment
+     *
+     * @param Issue $issue
+     */
+    public function logPersistComment(Comment $comment)
+    {
+        $message = 'New comment added';
+        $this->addActivity($message, $comment->getIssue(), $this->getUser());
+    }
+
+    /**
+     * add an activity entity
+     *
+     * @param string $message
+     * @param Issue $issue
+     * @param User $user
+     */
+    public function addActivity($message, Issue $issue, User $user)
+    {
+        $em = $this->getEntityManager();
+
+        $activity = new Activity();
+
+        $activity->setMessage($message);
+        $activity->setIssue($issue);
+        $activity->setUser($user);
+
+        $this->activities[] = $activity;
+    }
+
+    /**
+     *  Save activities
+     */
+    public function saveActivities()
+    {
+        if (!empty($this->activities)) {
+            $em = $this->getEntityManager();
+
+            foreach ($this->activities as $activity) {
+                $em->persist($activity);
+            }
+
+            $this->activities = [];
+            $em->flush();
+        }
+    }
+}

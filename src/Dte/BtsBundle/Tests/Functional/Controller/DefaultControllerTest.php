28c6beec6dad77102ab89cca57c885af4390defb
Denys Temchenko <denis.temchenko@gmail.com>
2015-02-20 13:53:00 +0000

    tests updated

--- src/Dte/BtsBundle/Tests/Functional/Controller/DefaultControllerTest.php
+++ src/Dte/BtsBundle/Tests/Functional/Controller/DefaultControllerTest.php
@@ -27,7 +27,7 @@ class DefaultControllerTest extends FixturesWebTestCase
         $this->assertFalse($this->client->getResponse() instanceof RedirectResponse);
     }
 
-    public function testLoginForm()
+    public function testLoginLogoutScenario()
     {
         $crawler = $this->client->request('GET', '/login');
 
@@ -38,5 +38,24 @@ class DefaultControllerTest extends FixturesWebTestCase
         $this->assertTrue($crawler->filter('input[name="_password"]')->count() > 0);
 
         $this->assertTrue($crawler->filter('button[type=submit]')->count() > 0);
+
+        $form = $crawler->selectButton('Login')->form(array(
+            '_username'  => 'admin',
+            '_password'  => 'admin',
+        ));
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertGreaterThan(0, $crawler->selectLink('Admin A.A.')->count());
+
+        $crawler = $this->client->click($crawler->selectLink('Log Out')->link());
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertTrue($this->client->getResponse() instanceof RedirectResponse);
+
+        $this->assertRegExp('/\/login$/', $this->client->getResponse()->headers->get('location'));
     }
+
 }
--- src/Dte/BtsBundle/Tests/Functional/Controller/IssueControllerTest.php
+++ src/Dte/BtsBundle/Tests/Functional/Controller/IssueControllerTest.php
@@ -41,6 +41,15 @@ class IssueControllerTest extends FixturesWebTestCase
 
         $this->assertGreaterThan(0, $crawler->filter('#issue-code:contains("(BTS-8)")')->count(), 'Missing element #issue-code:contains("(BTS-8)")');
 
+        $this->assertEquals('btn btn-default  active disabled ', $crawler->selectLink('Open')->attr('class'));
+
+        $crawler = $this->client->click($crawler->selectLink('In progress')->link());
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals('btn btn-default ', $crawler->selectLink('Open')->attr('class'));
+        $this->assertEquals('btn btn-default  active disabled ', $crawler->selectLink('In progress')->attr('class'));
+
         $crawler = $this->client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Update')->form(array(
@@ -51,5 +60,30 @@ class IssueControllerTest extends FixturesWebTestCase
         $crawler = $this->client->followRedirect();
 
         $this->assertGreaterThan(0, $crawler->filter('[value="Test issue summary updated"]')->count(), 'Missing element [value="Test issue summary updated"]');
+    }
+
+    public function testAddSubtaskScenario()
+    {
+        $this->logInByUsername('admin');
+
+        $crawler = $this->client->request('GET', '/issue/1');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /issue/");
+        $crawler = $this->client->click($crawler->selectLink('Add Subtask')->link());
+
+        $form = $crawler->selectButton('Create')->form(array(
+            'dte_btsbundle_issue[project]'     => 1,
+            'dte_btsbundle_issue[type]'        => 4,
+            'dte_btsbundle_issue[summary]'     => 'Test issue summary',
+            'dte_btsbundle_issue[description]' => 'Test issue summary descrption',
+            'dte_btsbundle_issue[status]'      => 1,
+            'dte_btsbundle_issue[priority]'    => 3,
+            'dte_btsbundle_issue[assignee]'    => 1,
+            'dte_btsbundle_issue[parent]'      => 1,
+        ));
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertGreaterThan(0, $crawler->filter('#issue-code:contains("(BTS-8)")')->count(), 'Missing element #issue-code:contains("(BTS-8)")');
     }
 }
